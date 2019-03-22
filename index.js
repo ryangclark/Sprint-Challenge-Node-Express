84b3350f80359fb6c101e257de054f66c0580fa1
Clark <rclark010@gmail.com>
2019-03-22 09:54:43 -0600

    Added dotenv and set up index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+
+require('dotenv').config();
+
+const server = require('./server');
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () =>
+  console.log(`\n** Server Running on Port ${port}! **\n`)
+);
